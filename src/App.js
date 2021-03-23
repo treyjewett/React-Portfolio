bd31a632ca4bbf1833e4c65dd50dc48d7f4bb624
Russell Jewett <rfjewett3@gmail.com>
2021-03-22 21:42:27 -0400

    completes app.js and index.js files so the app will render.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./Pages/About";
+import Work from "./Pages/Work";
+import Contact from "./Pages/Contact";
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+          <Route exact path="/" component={About} />
+          <Route exact path="/projects" component={Work} />
+          <Route exact path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
 

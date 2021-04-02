533f2d5898e148975db03a96931ebaff5ad47888
Russell Jewett <rfjewett3@gmail.com>
2021-04-02 16:28:55 -0400

    adds styling to contact page

--- src/Components/ContactItem/index.js
+++ src/Components/ContactItem/index.js
@@ -5,9 +5,9 @@ function ContactItem(props) {
   return (
     <div className="contactItem">
       <ul className="contactList">
-        <li><a href={contact[0].source}>{contact[0].title}</a></li>
-        <li><a href={contact[1].source}>{contact[1].title}</a></li>
-        <li><a href={contact[2].source}>{contact[2].title}</a></li>
+        <li>Resume: <a href={contact[0].source}>{contact[0].title}</a></li>
+        <li>GitHub: <a href={contact[1].source}>{contact[1].title}</a></li>
+        <li>LinkedIn: <a href={contact[2].source}>{contact[2].title}</a></li>
         <li>{contact[3].title}: {contact[3].source}</li>
         <li>{contact[4].title}: {contact[4].source}</li>
       </ul>

50318cf2e35ea03f92b9a3d715c2441b234c99e6
Russell Jewett <rfjewett3@gmail.com>
2021-04-14 22:00:40 -0400

    updates information to reflect latest work

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -11,7 +11,7 @@ function About() {
       <h1 className="about">About Me</h1>
       <div className="aboutContainer">
         <img id="profile" src={process.env.PUBLIC_URL + "/assets/img/profile.jpg"} alt="Myself currently" />
-        <p id="blurb">Raised in Charlotte, North Carolina, I have remained close to home and I now live in Dallas, North Carolina.
+        <p id="blurb">Raised in Charlotte, North Carolina, I have remained close to home and I now live in Kernersville, North Carolina.
         I graduated from UNC Charlotte in 2017 after obtaining a bachelors degree in exercise physiology.
         I currently work for a research and development facility where knowledge regarding technology is required.
         I am attending UNC Charlotte's coding boot camp in an attempt to further my understanding of applications and how to apply them to my line of work.
@@ -20,8 +20,9 @@ function About() {
         Soon after graduating, I realized that the field was not for me and I have been in engineering ever since.
         Having worked in the field for over 3 years, I gravitated towards technology.
         It quickly became apparent that I needed some education in the "behind-the-scenes" systems that help technology perform.
-        After nearly 18 months of teaching myself Python, I realized that I needed a strong foundation in order to build my education efficiently.
-        After the bootcamp, I want to utilize my education to advance machine learning and other AI applications.</p>
+        After nearly 18 months of teaching myself Python, I realized that I needed a strong foundation in order to build my education efficiently. I enrolled in 
+        UNC Charlotte's Full Stack Developer Bootcamp and learned the skills that I was missing. Having graduated, I am eager to put my skillset to use on 
+        real-world applications.</p>
       </div>
       <Footer />
     </div>

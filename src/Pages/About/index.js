cd49d9209af78c0f7953bb73bceaa0433b3f7a8a
Russell Jewett <rfjewett3@gmail.com>
2024-08-29 11:05:57 -0400

    updates to about blurb

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -13,7 +13,7 @@ function About() {
         <img id="profile" src={process.env.PUBLIC_URL + "/assets/img/profile.jpg"} alt="Myself currently" />
         <p id="blurb">Raised in Charlotte, North Carolina, I have remained close to home and I now live in Mooresville, North Carolina.
           I completed my Full Stack Web Development certification from UNC Charlotte in April 2021 and have been in the field ever since.
-          I am currently employed at ImagineSoftware located in Charlotte as a Full Stack Software Engineer. I graduated from UNC Charlotte
+          I am currently employed at ImagineSoftware located in Charlotte as a Full Stack Software Engineer focused in .NET. I graduated from UNC Charlotte
           in 2017 after obtaining a bachelors degree in exercise physiology. When I was younger, I had always wanted to be an engineer. Playing
           sports throughout my childhood really gave me a passion for physiology, which is why I chose to study that in college. Soon after
           graduating, I realized that the field was not for me and I have been in engineering ever since.</p>
